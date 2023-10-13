33c5b94aed052353ab2ada47606704d5d1d9a3fc
HaithamAhmed01 <54605034+HaithamAhmed01@users.noreply.github.com>
2023-10-13 08:08:20 +0300

    fix use case

--- lib/Features/home/domain/use_cases/fetch_featured_books_use_case.dart
+++ lib/Features/home/domain/use_cases/fetch_featured_books_use_case.dart
@@ -1,14 +1,20 @@
 import 'package:bookly/Features/home/domain/entities/book_entity.dart';
 import 'package:bookly/Features/home/domain/repo/home_repo.dart';
 import 'package:bookly/core/errors/failure.dart';
+import 'package:bookly/core/use_cases/use_case.dart';
 import 'package:dartz/dartz.dart';
 
-class FetchFeaturedBooksUseCase {
+class FetchFeaturedBooksUseCase extends UseCase< List<BookEntity>,int> {
   final HomeRepo homeRepo;
 
   FetchFeaturedBooksUseCase(this.homeRepo);
 
-  Future<Either<Failure, List<BookEntity>>> fetchFeaturedBooks() {
-    return homeRepo.fetchFeaturedBooks();
+  @override
+  Future<Either<Failure, List<BookEntity>>> call([int param = 0]) async {
+    return await homeRepo.fetchFeaturedBooks(
+        // pageNumber: param,
+        );
   }
+
+ 
 }
--- lib/Features/home/domain/use_cases/fetch_newest_books_use_case.dart
+++ lib/Features/home/domain/use_cases/fetch_newest_books_use_case.dart
@@ -1,17 +1,18 @@
 import 'package:bookly/Features/home/domain/repo/home_repo.dart';
+import 'package:bookly/core/use_cases/use_case.dart';
 import 'package:dartz/dartz.dart';
 
 import '../../../../core/errors/failure.dart';
 
 import '../entities/book_entity.dart';
 
-class FetchNewestBooksUseCase {
+class FetchNewestBooksUseCase extends UseCase<List<BookEntity>, NoParam> {
   final HomeRepo homeRepo;
 
   FetchNewestBooksUseCase(this.homeRepo);
 
   @override
-  Future<Either<Failure, List<BookEntity>>> fetchNewestBooks() {
-    return homeRepo.fetchNewestBooks();
+  Future<Either<Failure, List<BookEntity>>> call([NoParam? param]) async {
+    return await homeRepo.fetchNewestBooks();
   }
 }
