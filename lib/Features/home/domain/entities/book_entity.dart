cde48f763b66298b0f1fedd5510ee8bc645e851b
HaithamAhmed01 <54605034+HaithamAhmed01@users.noreply.github.com>
2023-10-13 07:29:49 +0300

    add domain

---
+++ lib/Features/home/domain/entities/book_entity.dart
@@ -0,0 +1,16 @@
+class BookEntity {
+  final String bookId;
+  final String? image;
+  final String title;
+  final String? authorName;
+  final num? price;
+  final num? rating;
+
+  BookEntity(
+      {required this.bookId,
+      required this.image,
+      required this.title,
+      required this.authorName,
+      required this.price,
+      required this.rating});
+}
---
+++ lib/Features/home/domain/repo/home_repo.dart
@@ -0,0 +1,7 @@
+import 'package:bookly/Features/home/domain/entities/book_entity.dart';
+
+abstract class HomeRepo {
+  Future<List<BookEntity>> fetchFeaturedBooks();
+
+  Future<List<BookEntity>> fetchNewestBooks();
+}
